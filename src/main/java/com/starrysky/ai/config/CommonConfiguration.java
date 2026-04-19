f67a2c1e9b240e2060a62b3c0defa9f12c3dbdfb
StarrySky <3092692014@qq.com>
2026-04-19 16:49:09 +0800

    配置模型

---
+++ src/main/java/com/starrysky/ai/config/CommonConfiguration.java
@@ -0,0 +1,25 @@
+package com.starrysky.ai.config;
+
+import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.ai.ollama.OllamaChatModel;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
+
+/**
+ * @author LIU
+ */
+@Configuration
+public class CommonConfiguration {
+
+    /*
+     * 配置模型，也可以用其他的模型，openai的
+     */
+    @Bean
+    public ChatClient chatClient(OllamaChatModel model){
+        return ChatClient
+                .builder(model)
+                .defaultSystem("你是一个热心的智能助手，你的名字叫做小团团。请用温柔可爱的语气回答用户。")
+                .build();
+    }
+}
