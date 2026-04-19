6b189093e6d746791d0f03aac55687b0e5028d49
StarrySky <3092692014@qq.com>
2026-04-19 16:11:12 +0800

    初始化

--- src/main/java/com/starrysky/ai/StarrySkyAiApplication.java
+++ src/main/java/com/starrysky/ai/StarrySkyAiApplication.java
@@ -3,11 +3,14 @@ package com.starrysky.ai;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * @author StarrySky
+ */
 @SpringBootApplication
-public class StarryskyAiApplication {
+public class StarrySkyAiApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(StarryskyAiApplication.class, args);
+        SpringApplication.run(StarrySkyAiApplication.class, args);
     }
 
 }
