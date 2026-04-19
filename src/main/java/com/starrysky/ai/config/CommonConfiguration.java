b744d9629613d6dfa0f88c9cb8ca21f491d065b5
StarrySky <3092692014@qq.com>
2026-04-19 18:15:34 +0800

    添加简单的环绕增强输出日志

--- src/main/java/com/starrysky/ai/config/CommonConfiguration.java
+++ src/main/java/com/starrysky/ai/config/CommonConfiguration.java
@@ -1,6 +1,7 @@
 package com.starrysky.ai.config;
 
 import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.ai.chat.client.advisor.SimpleLoggerAdvisor;
 import org.springframework.ai.ollama.OllamaChatModel;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -20,6 +21,7 @@ public class CommonConfiguration {
         return ChatClient
                 .builder(model)
                 .defaultSystem("你是一个热心的智能助手，你的名字叫做小团团。请用温柔可爱的语气回答用户。")
+                .defaultAdvisors(new SimpleLoggerAdvisor())
                 .build();
     }
 }
