6e3fddafc0b08c35c9968b972437edd71f0f9aac
StarrySky <3092692014@qq.com>
2026-04-19 19:05:59 +0800

    增加内存存储记忆功能

--- src/main/java/com/starrysky/ai/config/CommonConfiguration.java
+++ src/main/java/com/starrysky/ai/config/CommonConfiguration.java
@@ -1,7 +1,10 @@
 package com.starrysky.ai.config;
 
 import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.ai.chat.client.advisor.MessageChatMemoryAdvisor;
 import org.springframework.ai.chat.client.advisor.SimpleLoggerAdvisor;
+import org.springframework.ai.chat.memory.ChatMemory;
+import org.springframework.ai.chat.memory.MessageWindowChatMemory;
 import org.springframework.ai.ollama.OllamaChatModel;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -17,12 +20,24 @@ public class CommonConfiguration {
      * 配置模型，也可以用其他的模型，openai的
      */
     @Bean
-    public ChatClient chatClient(OllamaChatModel model){
+    public ChatClient chatClient(OllamaChatModel model,ChatMemory chatMemory){
         return ChatClient
                 .builder(model)
                 .defaultSystem("你是一个热心的智能助手，你的名字叫做小团团。请用温柔可爱的语气回答用户。")
-                .defaultAdvisors(new SimpleLoggerAdvisor())
-                .defaultAdvisors(new SimpleLoggerAdvisor()) //环绕日志增强
+                .defaultAdvisors(
+                        new SimpleLoggerAdvisor(), //环绕日志增强
+                        MessageChatMemoryAdvisor.builder(chatMemory).build()//环绕记忆增强
+                        )
                 .build();
+    }
+
+    /*
+     * 配置记忆库，使用默认实现的内存存储记忆
+     *
+     * 也可以实现ChatMemory接口，实现add，get，clear方法，自定义存储记忆的方式
+     */
+    @Bean
+    public ChatMemory chatMemory(){
+        return MessageWindowChatMemory.builder().build();
     }
 }
