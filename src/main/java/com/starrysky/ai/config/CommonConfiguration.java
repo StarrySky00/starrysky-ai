71052e2081a7c38ec12100daa0b673e84a7f96c3
StarrySky <3092692014@qq.com>
2026-04-20 21:18:03 +0800

    测试向量模型

--- src/main/java/com/starrysky/ai/config/CommonConfiguration.java
+++ src/main/java/com/starrysky/ai/config/CommonConfiguration.java
@@ -7,8 +7,12 @@ import org.springframework.ai.chat.client.advisor.MessageChatMemoryAdvisor;
 import org.springframework.ai.chat.client.advisor.SimpleLoggerAdvisor;
 import org.springframework.ai.chat.memory.ChatMemory;
 import org.springframework.ai.chat.memory.MessageWindowChatMemory;
+import org.springframework.ai.observation.conventions.VectorStoreProvider;
 import org.springframework.ai.ollama.OllamaChatModel;
 import org.springframework.ai.openai.OpenAiChatModel;
+import org.springframework.ai.openai.OpenAiEmbeddingModel;
+import org.springframework.ai.vectorstore.SimpleVectorStore;
+import org.springframework.ai.vectorstore.VectorStore;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
@@ -24,20 +28,20 @@ public class CommonConfiguration {
      */
     // 本地部署
     @Bean
-    public ChatClient chatClient(OllamaChatModel model,ChatMemory chatMemory){
+    public ChatClient chatClient(OllamaChatModel model, ChatMemory chatMemory) {
         return ChatClient
                 .builder(model)
                 .defaultSystem("你是一个热心的智能助手，你的名字叫做小团团。请用温柔可爱的语气回答用户。")
                 .defaultAdvisors(
                         new SimpleLoggerAdvisor(), //环绕日志增强
                         MessageChatMemoryAdvisor.builder(chatMemory).build()//环绕记忆增强
-                        )
+                )
                 .build();
     }
 
     // 游戏模型配置
     @Bean
-    public ChatClient gameChatClient(OpenAiChatModel model, ChatMemory chatMemory){
+    public ChatClient gameChatClient(OpenAiChatModel model, ChatMemory chatMemory) {
         return ChatClient
                 .builder(model)
                 .defaultSystem(SystemConstants.GAME_SYSTEM_PROMPT)
@@ -50,7 +54,7 @@ public class CommonConfiguration {
 
     // 客服模型配置
     @Bean
-    public ChatClient serviceChatClient(OpenAiChatModel model, ChatMemory chatMemory, CourseTools tools){
+    public ChatClient serviceChatClient(OpenAiChatModel model, ChatMemory chatMemory, CourseTools tools) {
         return ChatClient
                 .builder(model)
                 .defaultSystem(SystemConstants.SERVICE_SYSTEM_PROMPT)//系统角色
@@ -62,13 +66,19 @@ public class CommonConfiguration {
                 .build();
     }
 
+    //向量库和向量模型配置
+    @Bean
+    public VectorStore vectorStore(OpenAiEmbeddingModel embeddingModel) {
+        return SimpleVectorStore.builder(embeddingModel).build();
+    }
+
     /*
      * 配置记忆库，使用默认实现的内存存储记忆
      *
      * 也可以实现ChatMemory接口，实现add，get，clear方法，自定义存储记忆的方式
      */
     @Bean
-    public ChatMemory chatMemory(){
+    public ChatMemory chatMemory() {
         return MessageWindowChatMemory.builder().build();
     }
 }
--- src/test/java/com/starrysky/ai/StarrySkyAiApplicationTests.java
+++ src/test/java/com/starrysky/ai/StarrySkyAiApplicationTests.java
@@ -1,22 +1,58 @@
 package com.starrysky.ai;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.ai.openai.OpenAiEmbeddingModel;
+import org.springframework.ai.document.Document;
+import org.springframework.ai.reader.ExtractedTextFormatter;
+import org.springframework.ai.reader.pdf.PagePdfDocumentReader;
+import org.springframework.ai.reader.pdf.config.PdfDocumentReaderConfig;
+import org.springframework.ai.vectorstore.SearchRequest;
+import org.springframework.ai.vectorstore.VectorStore;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.core.io.FileSystemResource;
 
-import java.util.Arrays;
+import java.util.List;
 
 @SpringBootTest
 class StarrySkyAiApplicationTests {
 
     @Autowired
-    private OpenAiEmbeddingModel embeddingModel;
+    private VectorStore vectorStore;
+
+
 
     @Test
-    void contextLoads() {
-        float[] floats = embeddingModel.embed("学Spring Ai就到哔哩哔哩");
-        System.out.println(Arrays.toString(floats));
+    public void testVectorStore() {
+        // 1. 创建一个PDF文件路径
+        FileSystemResource resource = new FileSystemResource("交通运输生涯发展报告.pdf");
+        // 2. 创建一个PDF文件读取器
+        //resource:"classpath:/sample1.pdf"
+        PagePdfDocumentReader pdfReader = new PagePdfDocumentReader(resource,
+                PdfDocumentReaderConfig.builder()
+                        .withPageExtractedTextFormatter(ExtractedTextFormatter.defaults())//文本格式化器
+                        .withPagesPerDocument(1)//每几页作为一个document
+                        .build());
+        // 3. 读取pdf，拆分成document
+        List<Document> documents = pdfReader.read();
+        // 4. 写入向量数据库
+        vectorStore.add(documents);
+        // 5. 搜索
+        SearchRequest request = SearchRequest.builder()
+                .query("交通运输生涯如何扩展行业认知")
+                .topK(1) //前几页
+                .similarityThreshold(0.5) //评分阈值，大于几
+                .filterExpression("file_name == '交通运输生涯发展报告.pdf'")//过滤仅查看符合条件的
+                .build();
+        List<Document> docs = vectorStore.similaritySearch(request);
+
+
+        for (Document doc : docs) {
+            System.out.println(doc.getId());
+            System.out.println(doc.getScore());
+            System.out.println(doc.getText());
+        }
+
+
     }
 
 }
