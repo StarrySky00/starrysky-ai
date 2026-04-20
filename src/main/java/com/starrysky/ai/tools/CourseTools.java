afbae44b5224001fd65b5c200ba3f827bb7f885b
StarrySky <3092692014@qq.com>
2026-04-20 14:05:43 +0800

    根据条件查询课程列表工具

--- src/main/java/com/starrysky/ai/tools/CourseTools.java
+++ src/main/java/com/starrysky/ai/tools/CourseTools.java
@@ -1,13 +1,48 @@
 package com.starrysky.ai.tools;
 
+import com.baomidou.mybatisplus.extension.conditions.query.LambdaQueryChainWrapper;
+import com.baomidou.mybatisplus.extension.conditions.query.QueryChainWrapper;
+import com.starrysky.ai.entity.po.Course;
+import com.starrysky.ai.entity.query.CourseQuery;
+import com.starrysky.ai.service.ICourseReservationService;
+import com.starrysky.ai.service.ICourseService;
+import com.starrysky.ai.service.ISchoolService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.ai.tool.annotation.Tool;
+import org.springframework.ai.tool.annotation.ToolParam;
 import org.springframework.stereotype.Component;
+import java.util.List;
 
 /**
  * @author StarrySky
- * @date 2026/4/20 11:29 星期一
+ * &#064;date  2026/4/20 11:29 星期一
  */
 
 @Component
+@RequiredArgsConstructor
 public class CourseTools {
+    private final ICourseService courseService;
+    private final ICourseReservationService courseReservationService;
+    private final ISchoolService schoolService;
+
+    //根据query条件查询课程列表
+    //returnDirect = false  结果返回给前端or大模型。默认false返回给大模型
+    @Tool(description = "根据条件查询课程列表", returnDirect = false)
+    public List<Course> queryCourse(@ToolParam(description = "查询的条件") CourseQuery query) {
+        if(query==null){
+            return List.of();
+        }
+        QueryChainWrapper<Course> wrapper = courseService.query()
+                .eq(query.getType() != null, "type", query.getType())
+                .le(query.getEdu() != null, "edu", query.getEdu());
+        if(query.getSorts()!=null && !query.getSorts().isEmpty()){
+            for (CourseQuery.Sort sort : query.getSorts()) {
+                wrapper.orderBy(true, sort.getAsc(), sort.getField());
+            }
+        }
+        return wrapper.list();
+    }
+
+
 
 }
