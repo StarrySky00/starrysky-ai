2fd806a2ea8b5c3a893dac406153e713e494ebc6
StarrySky <3092692014@qq.com>
2026-04-20 11:30:27 +0800

    学习functionCalling初始化，创建所需类

---
+++ src/main/java/com/starrysky/ai/tools/CourseTools.java
@@ -0,0 +1,13 @@
+package com.starrysky.ai.tools;
+
+import org.springframework.stereotype.Component;
+
+/**
+ * @author StarrySky
+ * @date 2026/4/20 11:29 星期一
+ */
+
+@Component
+public class CourseTools {
+
+}
