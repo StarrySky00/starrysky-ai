2edea70a50d2e9f216e612f28a9f018f895e17a8
StarrySky <3092692014@qq.com>
2026-04-20 14:25:28 +0800

    生成预约单

--- src/main/java/com/starrysky/ai/tools/CourseTools.java
+++ src/main/java/com/starrysky/ai/tools/CourseTools.java
@@ -3,6 +3,7 @@ package com.starrysky.ai.tools;
 import com.baomidou.mybatisplus.extension.conditions.query.LambdaQueryChainWrapper;
 import com.baomidou.mybatisplus.extension.conditions.query.QueryChainWrapper;
 import com.starrysky.ai.entity.po.Course;
+import com.starrysky.ai.entity.po.CourseReservation;
 import com.starrysky.ai.entity.po.School;
 import com.starrysky.ai.entity.query.CourseQuery;
 import com.starrysky.ai.service.ICourseReservationService;
@@ -30,7 +31,7 @@ public class CourseTools {
     //根据query条件查询课程列表
     //returnDirect = false  结果返回给前端or大模型。默认false返回给大模型
     @Tool(description = "根据条件查询课程列表", returnDirect = false)
-    public List<Course> queryCourse(@ToolParam(description = "查询的条件") CourseQuery query) {
+    public List<Course> queryCourse(@ToolParam(description = "查询的条件", required = false) CourseQuery query) {
         if (query == null) {
             return List.of();
         }
@@ -47,10 +48,28 @@ public class CourseTools {
 
     // 查询校区
     @Tool(description = "根据条件查询所有校区")
-    public List<School> querySchool(@ToolParam(description = "校区所在城市集合") List<School> cities) {
+    public List<School> querySchool(@ToolParam(description = "校区所在城市集合", required = false) List<School> cities) {
         return schoolService.lambdaQuery()
                 .in(cities != null && cities.isEmpty(), School::getCity, cities)
                 .list();
+    }
+
+    //预约课程和对应校区
+    @Tool(description = "生成预约单,返回预约单号预提示信息")
+    public String reserveCourse(@ToolParam(description = "预约的课程") String course,
+                                @ToolParam(description = "预约的校区") String school,
+                                @ToolParam(description = "预约学生姓名") String studentName,
+                                @ToolParam(description = "预约学生联系方式") String contactInfo,
+                                @ToolParam(description = "备注", required = false) String remark
+    ) {
+        CourseReservation cr = new CourseReservation();
+        cr.setCourse(course);
+        cr.setSchool(school);
+        cr.setStudentName(studentName);
+        cr.setContactInfo(contactInfo);
+        cr.setRemark(remark);
+        return courseReservationService.save(cr)
+                ? String.format("预约成功【预约单号：%s】", cr.getId()) : "预约失败";
     }
 
 
