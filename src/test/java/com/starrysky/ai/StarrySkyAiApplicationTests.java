92d4724d8a6b8eca8b4c56c30d9a5ee7983d6ac2
StarrySky <3092692014@qq.com>
2026-04-20 20:07:25 +0800

    测试向量模型

--- src/test/java/com/starrysky/ai/StarrySkyAiApplicationTests.java
+++ src/test/java/com/starrysky/ai/StarrySkyAiApplicationTests.java
@@ -1,13 +1,22 @@
 package com.starrysky.ai;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.ai.openai.OpenAiEmbeddingModel;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.Arrays;
 
 @SpringBootTest
 class StarrySkyAiApplicationTests {
 
+    @Autowired
+    private OpenAiEmbeddingModel embeddingModel;
+
     @Test
     void contextLoads() {
+        float[] floats = embeddingModel.embed("学Spring Ai就到哔哩哔哩");
+        System.out.println(Arrays.toString(floats));
     }
 
 }
